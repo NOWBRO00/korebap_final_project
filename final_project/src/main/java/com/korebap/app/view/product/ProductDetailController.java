0ffb303aa1da8600401b5680222afeafc8697737
jejuorangee <idjjh1998@gmail.com>
2024-11-07 14:58:44 +0900

    고래밥 최종프로젝트 1106.5

--- final_project/src/main/java/com/korebap/app/view/product/ProductDetailController.java
+++ final_project/src/main/java/com/korebap/app/view/product/ProductDetailController.java
@@ -19,72 +19,82 @@ import com.korebap.app.biz.wishlist.WishlistDTO;
 
 @Controller
 public class ProductDetailController {
-	
-	@Autowired
-	private ProductService productService;
-	
-	@Autowired
-	private ReviewService reviewService;
-	
-	@Autowired
-	private ImageFileService fileService;
 
-	@GetMapping(value="/productDetail.do")
-	public String productDetail(Model model,ProductDTO productDTO, 
-			ReviewDTO reviewDTO,ImageFileDTO fileDTO, WishlistDTO wishlistDTO) {
+   @Autowired
+   private ProductService productService;
 
-		// [ 상품 상세보기 ]
-		System.out.println("************************************************************[com.korebap.app.view.product productDetail 시작]************************************************************");
+   @Autowired
+   private ReviewService reviewService;
 
-		// 경로를 담을 변수 설정
-		String viewName;
-		
-		// 상품 번호
-		int product_num = productDTO.getProduct_num();
-		
-		// 데이터 로그
-		System.out.println("*****com.korebap.app.view.product product_num ["+product_num+"]*****");
+   @Autowired
+   private ImageFileService fileService;
 
-		
-		// [상품 리스트] 반환
-		productDTO.setProduct_condition("PRODUCT_BY_INFO");
-		productDTO = productService.selectOne(productDTO);
-		
-		// [파일 리스트] 반환
-		fileDTO.setFile_product_num(product_num);
-		fileDTO.setFile_condition("PRODUCT_FILE_SELECTALL");
-		List<ImageFileDTO> fileList = fileService.selectAll(fileDTO);
+   @GetMapping(value="/productDetail.do")
+   public String productDetail(Model model,ProductDTO productDTO, 
+         ReviewDTO reviewDTO,ImageFileDTO fileDTO, WishlistDTO wishlistDTO) {
 
-		// [리뷰 리스트] 반환
-		reviewDTO.setReview_product_num(product_num);
-		System.out.println("reviewDTO 확인 : "+reviewDTO+"]");
-		List<ReviewDTO> reviewList = reviewService.selectAll(reviewDTO);
+      // [ 상품 상세보기 ]
+      System.out.println("************************************************************[com.korebap.app.view.product productDetail 시작]************************************************************");
 
-		
-		// 만약 productDTO 객체가 있다면
-		if(productDTO != null) {
-			System.out.println("*****com.korebap.app.view.product productDetail 상품 상세 있음 *****");
+      // 경로를 담을 변수 설정
+      String viewName;
 
-			model.addAttribute("product", productDTO);
-			model.addAttribute("fileList", fileList);
-			model.addAttribute("reviewList", reviewList);
-			
-			viewName = "productDetail";
-		}
-		else {
-			System.out.println("*****com.korebap.app.view.product productDetail 상품 상세 없음 *****");
-			
-			model.addAttribute("msg", "상품을 찾을 수 없습니다. 다시 시도해 주세요.");
-			model.addAttribute("path", "productList.do");
-			
-			viewName = "info";
-			
-		}
-		System.out.println("*****com.korebap.app.view.product productDetail viewName ["+ viewName +"]*****");
-		
-		System.out.println("************************************************************[com.korebap.app.view.product productDetail 종료]************************************************************");
+      // 상품 번호
+      int product_num = productDTO.getProduct_num();
 
-		return viewName;
-	}
+      // 데이터 로그
+      System.out.println("*****com.korebap.app.view.product product_num ["+product_num+"]*****");
+
+
+      // [상품] 반환
+      productDTO.setProduct_condition("PRODUCT_BY_INFO");
+      productDTO = productService.selectOne(productDTO);
+
+      // 주소 정보 분리
+      String[] totalAddress = productDTO.getProduct_address().split("_");
+
+      //productDTO.setProduct_postcode(totalAddress[0]); // 우편번호
+      String address = totalAddress[1]; // 기본 주소
+      String extraAddress = totalAddress[2]; // 추가 주소
+      //productDTO.setProduct_detailAddress(totalAddress[3]); // 상세 주소
+
+      productDTO.setProduct_address(address + extraAddress);
+
+      // [파일 리스트] 반환
+      fileDTO.setFile_product_num(product_num);
+      fileDTO.setFile_condition("PRODUCT_FILE_SELECTALL");
+      List<ImageFileDTO> fileList = fileService.selectAll(fileDTO);
+
+      // [리뷰 리스트] 반환
+      reviewDTO.setReview_product_num(product_num);
+      System.out.println("reviewDTO 확인 : "+reviewDTO+"]");
+      List<ReviewDTO> reviewList = reviewService.selectAll(reviewDTO);
+
+
+      // 만약 productDTO 객체가 있다면
+      if(productDTO != null) {
+         System.out.println("*****com.korebap.app.view.product productDetail 상품 상세 있음 *****");
+
+         model.addAttribute("product", productDTO);
+         model.addAttribute("fileList", fileList);
+         model.addAttribute("reviewList", reviewList);
+
+         viewName = "productDetail";
+      }
+      else {
+         System.out.println("*****com.korebap.app.view.product productDetail 상품 상세 없음 *****");
+
+         model.addAttribute("msg", "상품을 찾을 수 없습니다. 다시 시도해 주세요.");
+         model.addAttribute("path", "productList.do");
+
+         viewName = "info";
+
+      }
+      System.out.println("*****com.korebap.app.view.product productDetail viewName ["+ viewName +"]*****");
+
+      System.out.println("************************************************************[com.korebap.app.view.product productDetail 종료]************************************************************");
+
+      return viewName;
+   }
 
 }
